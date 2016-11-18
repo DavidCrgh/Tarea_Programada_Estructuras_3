a8ecc13d814267dc068d7e6d3f5fa496f35298b8
Bryan <mena97villalobos@gmail.com>
2016-11-18 13:26:47 -0600

    Clases provisionales 2

--- src/Utilitarias/Bomba.java
+++ src/Utilitarias/Bomba.java
@@ -12,30 +12,45 @@ public class Bomba{
         costo=2000;
     }
 
+    private void disparar(Matriz tableroJuego, int fila, int columna){
+        Random random=new Random();
+        if(random.nextBoolean()){
+            tableroJuego.tablero[fila][columna]=5;
+            tableroJuego.tablero[fila+1][columna]=5;
+        }
+        else{
+            tableroJuego.tablero[fila][columna]=5;
+            tableroJuego.tablero[fila][columna+1]=5;
+        }
+    }
+
     public String explotar(Matriz tableroJuego, int fila1, int fila2, int fila3,
                            int columna1, int columna2, int columna3){
-        Random random=new Random();
         String tmp="";
         if(tableroJuego.tablero[fila1][columna1]==4){
-
+            tableroJuego.hoyoNegro.onHit();
         }
         else {
             if (tableroJuego.tablero[fila1][columna1] != -1) {
-                if(random.nextBoolean()){
-                    tableroJuego.tablero[fila1][columna1]=5;
-                    tableroJuego.tablero[fila1+1][columna1]=5;
-                }
-                else{
-                    tableroJuego.tablero[fila1][columna1]=5;
-                    tableroJuego.tablero[fila1][columna1+1]=5;
-                }
+                disparar(tableroJuego, fila1, columna1);
                 tmp += "Bomba1 ha estallado un elemento enemigo";
+            }
+            else{
+                tmp += "Bomba1 ha fallado";
             }
             if (tableroJuego.tablero[fila2][columna2] != -1) {
-                tmp += "Bomba1 ha estallado un elemento enemigo";
+                disparar(tableroJuego, fila2, columna2);
+                tmp += "Bomba2 ha estallado un elemento enemigo";
+            }
+            else{
+                tmp += "Bomba1 ha fallado";
             }
             if (tableroJuego.tablero[fila3][columna3] != -1) {
-                tmp += "Bomba1 ha estallado un elemento enemigo";
+                disparar(tableroJuego, fila3, columna3);
+                tmp += "Bomba3 ha estallado un elemento enemigo";
+            }
+            else{
+                tmp += "Bomba1 ha fallado";
             }
         }
         return tmp;
